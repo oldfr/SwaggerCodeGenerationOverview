53cb19970e97e1b3938ed74cb722655aff0a859e
ankitha <ankitha.codes@gmail.com>
2022-07-04 17:50:12 +0530

    initial commit

---
+++ src/main/java/com/example/swaggercodegenerationoverview/SwaggerCodeGenerationOverviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.swaggercodegenerationoverview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerCodeGenerationOverviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerCodeGenerationOverviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/swaggercodegenerationoverview/SwaggerCodeGenerationOverviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swaggercodegenerationoverview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerCodeGenerationOverviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
